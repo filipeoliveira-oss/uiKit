cae7db647632d99a35332286f7c9bf7d06afacda
Filipe Oliveira <filipeoliveira084@gmail.com>
2025-02-04 21:41:10 -0300

    add modal component and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react"
+import Modal from "./components/modal/modal"
 
 function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+	const [open, setOpen] = useState(false)
+
+	return (
+		<div className="">
+			<button onClick={() => setOpen(true)}>open dialog</button>
+
+			<Modal isOpen={open} onClose={() => setOpen(false)} title="123">
+				<div className="w-full h-full flex flex-col gap-4 overflow-auto">
+					{Array.from({ length: 400 }, (_, index) => index).map((each) => {
+						return (
+							<div className="flex flex-row gap-5">
+								<input className="w-96 h-8 bg-red-500" value={`this is a input ${each}`}/>
+								<input className="w-96 h-8 bg-red-500" value={`this is a input ${each}`}/>
+							</div>
+						)
+					})}
+				</div>
+			</Modal>
+		</div>
+	)
 }
 
 export default App
