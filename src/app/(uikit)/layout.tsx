c17a3d364444316a9961eb8e5027f1a95c6b313a
Filipe Oliveira <filipeoliveira084@gmail.com>
2025-06-23 19:58:22 -0300

    refactor: update layout components for improved structure and styling

--- src/app/(uikit)/layout.tsx
+++ src/app/(uikit)/layout.tsx
@@ -1,25 +1,16 @@
-// import Sidebar from "@/components/sidebar";
-// import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
 
-// export default function UiKitLayout({
-//     children,
-// }: Readonly<{
-//     children: React.ReactNode;
-// }>) {
-//     return (
-//         <html lang="en">
-//             <body className={`h-dvh w-dvw antialiased bg-background relative flex justify-center flex-col items-center  overflow-hidden `}>
-//                 <div className="w-[50vh] h-1/2 bg-gradient-to-tr from-[#aa4b6b] via-bg-[#6b6b83] to-[#3b8d99] absolute top-[10%] right-8 rounded-full blur-[120px] opacity-40"></div>
-//                 <div className="w-[50vh] h-1/2 bg-gradient-to-tr from-[#a8ff78] to-[#78ffd6] absolute bottom-[10%] left-8 rounded-full blur-[120px] opacity-30"></div>
-//                 <Header/>
-
-//                 <div className="w-[80%] h-[92%] backdrop-blur-lg text-white  flex flex-row ">
-//                     <Sidebar/>
-//                     <div className="w-full h-full pl-8  prose text-white!">
-//                         {children}
-//                     </div>
-//                 </div>
-//             </body>
-//         </html>
-//     );
-// }
+export default function UiKitLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="w-full mt-20 flex items-center justify-center" style={{height:'calc(100vh - 80px)'}}>
+            <div className="w-defaultWidth h-full overflow-x-hidden overflow-y-auto flex flex-row py-4">
+                <Sidebar/>
+                {children}
+            </div>
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`min-h-dvh w-dvw antialiased bg-lpbackground flex flex-col overflow-x-hidden text-white`}>
+			<body className={`min-h-dvh w-dvw antialiased bg-lpbackground flex flex-col overflow-x-hidden text-white items-center`}>
 				<Header/>
 				{children}
 			</body>
