9b30f6f0b875af3147d789badb3b495165a1e2fe
Filipe Oliveira <filipeoliveira084@gmail.com>
2025-06-23 21:21:43 -0300

    hooks rebranding

--- src/app/(uikit)/hooks/useDocumentTitle/page.tsx
+++ src/app/(uikit)/hooks/useDocumentTitle/page.tsx
@@ -17,21 +17,20 @@ useDocumentTitle`
     const code = `useDocumentTitle('${title}')`
 
     const deps = [
-        { name: "react", url: "https://www.npmjs.com/package/react" },
-        { name: "react-dom", url: "https://www.npmjs.com/package/react-dom" }
+        { name: "react", url: "https://www.npmjs.com/package/react" }
     ]
 
     return (
         <PageWrapper requirements={deps} title="UseDocumentTitle">
             <h1 className="text-4xl font-bold">useDocumentTitle</h1>
-            <span>useInputFocus is a hook that will change the window document title</span>
+            <span>useDocumentTitle is a hook that will change the window document title</span>
 
             <CodeBlock code={a} />
 
             <h2 className="text-3xl font-bold">Usage</h2>
-            <CodeBlock code={code} language='javascript'/>
+            <CodeBlock code={code} language='js'/>
 
-            <span>Change the document title bellow</span>
+            <span>Change the document title bellow:</span>
             <input value={title} onChange={(e) => setTitle(e.target.value)} className='w-full bg-zinc-50 text-black h-8 pl-2 rounded-lg outline-none' placeholder='Change the document title' />
 
         </PageWrapper>
--- src/app/(uikit)/layout.tsx
+++ src/app/(uikit)/layout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Sidebar from "@/components/sidebar";
 
 export default function UiKitLayout({
@@ -6,9 +7,9 @@ export default function UiKitLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="w-full mt-20 flex items-center justify-center" style={{height:'calc(100vh - 80px)'}}>
+        <div className="w-full mt-20 flex items-center justify-center" style={{ height: 'calc(100vh - 80px)' }}>
             <div className="w-defaultWidth h-full overflow-x-hidden overflow-y-auto flex flex-row py-4">
-                <Sidebar/>
+                <Sidebar />
                 {children}
             </div>
         </div>
